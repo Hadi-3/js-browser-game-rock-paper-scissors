8611a693e2b35ce505ba54fcffe1c07c15d1f0bd
hadi osama <hadiosama622@gmail.com>
2025-06-29 11:10:08 +0300

    create computer choice function

--- js/app.js
+++ js/app.js
@@ -19,6 +19,31 @@ console.log(scissorsBtnEl)
 
 /* -------------- Functions --------------*/
 
+function getComputerChoice() {
+    // generate a random number 0-2
+    const randomIndex = Math.floor(Math.random() * choice.length)
+ 
+    // select the item from the array
+    return choice[randomIndex]
+}
+
+// initialize game state
+function play() {
+    computerChoice = getComputerChoice()
+console.log(computerChoice)
+    // after updating state, render to html
+    render()
+
+}
+
+play()
+
+// updates our UI/html directly
+function render() {
+
+}
+
+
 
 
 /* -------------- Event Listeners --------------*/
