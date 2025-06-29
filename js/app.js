ab8cac519492facd4ab5131e806dee3b07403b6a
hadi osama <hadiosama622@gmail.com>
2025-06-29 10:44:38 +0300

    Cache buttons and create state variables

---
+++ js/app.js
@@ -0,0 +1,24 @@
+/* -------------- Constants --------------*/
+const choice = ['rock', 'paper', 'scissors']
+
+
+/* -------------- Variables --------------*/
+let msg
+let playerChoice
+let computerChoice
+
+
+
+/* -------------- Cached Element References --------------*/
+const rockBtnEl = document.querySelector('#rock')
+console.log(rockBtnEl)
+const paperBtnEl = document.querySelector('#paper')
+console.log(paperBtnEl)
+const scissorsBtnEl = document.querySelector('#scissors')
+console.log(scissorsBtnEl)
+
+/* -------------- Functions --------------*/
+
+
+
+/* -------------- Event Listeners --------------*/
